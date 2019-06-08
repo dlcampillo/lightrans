380e999037ce449bb79f8f1d89df7f9f5abdbae6
david <david.lozanoc@um.es>
2019-06-08 19:23:55 +0200

    Commit inicial

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,181 @@
+//  MIT License
+//
+//  Copyright (c) 2018 David Lozano Campillo
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#include <util/delay.h> // Biblioteca necesaria para emplear _delay_us()
+#include <IRremote.h>   // Biblioteca necesaria para la comunicación infrarroja
+
+#define BAUDRATE 2000000 // Baudrate en baudps
+#define RX A0 // Puerto receptor
+
+int const preamble[20] = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+int buffer_preamble[20];
+
+int val, flag;
+
+IRsend irsend;
+
+// Función que toma una cadena de bits como entrada
+// y devuelve el número entero correspondiente
+
+int bits_to_int(String bits) {
+  int value = 0;
+  for (int i = 0; i < bits.length(); i++) {
+    value *= 2;
+    if (bits.charAt(i) == '1') value++;
+  }
+  return value;
+}
+
+// Función requerida para demodular la señal manchester
+// Como se puede apreciar, la señal aparece invertida, por
+// lo que 1 es 01 y 0 es 10
+
+String demod_manchester(String string, int len) {
+  String data_demod = "";
+  for(int i = 0; i < len * 2; i += 2) {
+    if(string.charAt(i) == '1') {
+      data_demod += "0"; 
+    }
+    if(string.charAt(i) == '0') {
+      data_demod += "1";
+    }
+  }
+  return data_demod;
+}
+
+// Deteccion de la señal: Se detecta un cambio de la señal, el tiempo
+// que ha llevado ese cambio y si se trata de 1, 0, 11 o 00. Debido a que
+// sería costoso crear una variable para cada dato recogido, se ha
+// decidido por utilizar un puntero reutilizable en vez de una variable global
+
+void get_data(int *val, int *flag) {
+  int v = digitalRead(RX);
+  int dv = digitalRead(RX);
+  long t = micros();
+  
+  while(v == dv) {
+    dv = digitalRead(RX);
+  }
+
+  // Si v es diferente de dv se ha producido un cambio de señal,
+  // por lo que calculamos el tiempo que ha transcurrido
+  
+  int dt = micros() - t;
+
+
+  if(dt < 1000) {
+    *flag = 0; // 1 o 0
+  }
+  if(dt >= 1200 && dt < 3000) {
+    *flag = 1; // 11 o 00
+  }
+
+  _delay_us(100);
+    
+  *val = v;
+}
+
+// Función que "escucha" una cadena de cierta longitud y la devuelve
+
+String get_chain(int len) {
+  String data = "";
+  
+  for(int i = 0; i < (len * 2 + 1); i++) { // Multiplicamos la longitud por dos ya que la señal
+    get_data(&val, &flag);                 // viene codificada en manchester
+    if(flag == 1) {
+      data += String(val);
+      i++;
+    }
+    data += String(val);
+  }
+
+  _delay_us(1000);                         // Como se envia un "1" como espaciador entre caracter y
+                                           // carácter que tarda 1 ms, esperamos dicho tiempo
+  return data;
+}
+
+// Detección del preámbulo: Se crea un buffer circular para
+// compararlo con la cadena del preámbulo
+
+int detect_preamble() {
+  get_data(&val, &flag);
+  int check = 1;
+
+  if(flag == 0) {
+    for(int i = 0; i < 19; i++) {
+      buffer_preamble[i] = buffer_preamble[i+1]; // Si solo es 1 o 0 desplazar 1
+    }
+  }
+  else {
+    for(int i = 0; i < 18; i++) {
+      buffer_preamble[i] = buffer_preamble[i+2]; // Si es 11 o 00 desplazar 2
+    }
+    buffer_preamble[18] = val;
+  }
+  buffer_preamble[19] = val;
+  
+  for(int i = 0; i < 20; i++) {
+    if(buffer_preamble[i] != preamble[i]) {
+      check = 0;                                 // Si un carácter no coincide con el preambulo, no es este
+    }
+  }
+
+  _delay_us(500);
+
+  return check;
+}
+
+void setup() {
+  pinMode(RX, INPUT);               // Inicializar el pin RX (A0) para escuchar datos
+  Serial.begin(BAUDRATE);           // Iniciar comunicacion serial a BAUDRATE (2000000) baudios
+  
+  for(int i = 0; i < 20; i++) {
+    buffer_preamble[i] = 0;         // Llenar el buffer circular que se empleara en la deteccion del preambulo
+  }
+}
+
+void loop() {
+  if(detect_preamble()) {           // Si se ha detectado un preambulo
+    _delay_us(500);
+    String seq = get_chain(10);   // Obtener el numero de secuencia
+    String data[16];
+    String checksum;
+    byte sum = 0;
+    
+    for(int i = 0; i < 16; i++) {
+      data[i] = get_chain(8);     // Obtener 16 caracteres de 8 bits cada uno
+    }
+
+    checksum = get_chain(8);      // Obtener el checksum
+
+    for(int i = 0; i < 16; i++) {
+      sum += bits_to_int(demod_manchester(data[i], 8)); // Realizar la suma de comprobacion de los datos obtenidos
+    }
+    
+    if(bits_to_int(demod_manchester(checksum, 8)) == sum) {
+      for(int i = 0; i < 16; i++) {
+        Serial.print(char(bits_to_int(demod_manchester(data[i], 8)))); // Imprimir los caracteres obtenidos en el monitor serie
+      }
+      irsend.sendNEC(0xFF, 8);    // Enviar señal de confirmación al emisor
+    }
+  }
+}
---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,158 @@
+//  MIT License
+//
+//  Copyright (c) 2018 David Lozano Campillo
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#include <util/delay.h>
+#include <IRremote.h>
+
+#define BITRATE 500                          // Bitrate en bps
+#define TX 3                                 // Puerto digital para la transmision de datos
+
+long time_interval = 1000000 / BITRATE / 2 ; // Intervalo de tiempo entre los bits manchester
+
+int sequence = 0;
+
+IRrecv irrecv(11);                           // Objeto que manejará el receptor infrarrojos localizado
+decode_results results;                      // en el pin digital 11
+
+// Funcion que trasforma un entero a una cadena de bits de una longitud
+// determinada, indicada por el parametro s. Para ello emplea la funcion
+// bitRead(entero, pos) y suma el resultado a una string
+
+String int_to_bits(int integer, int s) {
+  String bits = "";
+  int len = bits.length();
+  for(int i = s - 1; i >= 0; i--) {
+    bits += bitRead(integer, i);
+  }
+  return bits;
+}
+
+// Funcion que toma una cadena de bits como entrada y produce
+// un entero como salida
+
+int bits_to_int(String bits) {
+  int value = 0;
+  for (int i = 0; i < bits.length(); i++) {
+    value *= 2;                            // En binario cada posicion es el doble que la anterior
+    if (bits.charAt(i) == '1') value++;    // Si hay  un uno, suma uno a value
+  }
+  return value;
+}
+
+// Funcion necesaria para transmitir los datos codificados en Manchester
+
+void transmit(String data) {
+  for(int i = 0; i <= data.length(); i++) {
+    if(data[i] == '0') {                  // Si la señal es un 0, emitir
+      digitalWrite(TX, LOW);              // 0 - 1
+      _delay_us(time_interval);
+      digitalWrite(TX, HIGH);
+      _delay_us(time_interval);
+    }
+    if(data[i] == '1') {                  // Si la señal es un 1, emitir
+      digitalWrite(TX, HIGH);             // 1 - 0
+      _delay_us(time_interval);
+      digitalWrite(TX, LOW);
+      _delay_us(time_interval);
+    }
+  }
+}
+
+// Funcion que transmite el preambulo (10 unos)
+
+void preamble() {
+  transmit("1111111111");
+}
+
+void setup() {
+  pinMode(TX, OUTPUT);
+  Serial.begin(115200);   // Inicializar comunicacion serial a 115200 baudios
+  irrecv.enableIRIn();    // Inicializar el modulo de tranmision de infrarrojos
+}
+
+void loop() {
+  TIMER_DISABLE_INTR;     // Desabilitar las interrupciones de la funcion enableIRIn()
+  
+  int new_package = 0;
+  byte sum = 0;
+  
+  String data[16];
+  String seq = int_to_bits(sequence, 10);  // Almacenar el numero de secuencia en binario
+  String checksum;
+
+  for(int i = 0; i < 15; i++) {
+    data[i] = int_to_bits(0, 8);           // Inicializar el array de datos con 0 (por si no se llena,
+  }                                        // que el resto sean ceros
+  
+  for(int i = 0; i < 16; i++) {            // Bucle para leer hasta 16 caracteres
+    if(Serial.available()) {               // Si se han recibido datos
+      char data_read = Serial.read();      // Leer dato
+      Serial.print(data_read);
+      
+      if(data_read == 10) {                    // Si se encuentra el caracter de salto de linea
+        data[15] = int_to_bits(data_read, 8);  // Insertar el caracter al final del array
+        break;                                 // Salir del bucle de llenado de caracter ya que el usuario ha pulsado enter
+      }
+      
+      data[i] = int_to_bits(data_read, 8);     // Llenar la posicion i con el elemento i-esimo
+    }
+    else {                                     // No se ha recibido caracter
+      transmit("101010");                      // Transmitir cadena de "idle"
+      return;                                  // Salir del bucle loop y volver a intentar recuperar datos
+    }
+  }
+
+  for(int i = 0; i < 16; i++) {
+    sum += bits_to_int(data[i]);               // Realizar la suma checksum
+  }
+
+  checksum = int_to_bits(sum, 8);              // Transformar la suma a cadena de bits
+
+  do {                                         // Transmitir el paquete al menos una vez
+    transmit("101010");                        // Es necesario un ciclo "idle" para que el receptor detecte el preambulo
+    
+    preamble();                                // Enviar preambulo
+    
+    transmit("1");                             // Separador
+    transmit(seq);                             // Enviar numero de secuencia
+  
+    for(int i = 0; i < 16; i++) {              // Enviar los 16 caracteres
+      transmit("1");                           // Separador
+      transmit(data[i]);                       // Dato i-esimo
+    }
+    
+    transmit("1");                             // Separador
+    transmit(checksum);                        // Enviar checksum
+    
+    transmit("0");                             // Enviar terminador de paquete
+
+    TIMER_ENABLE_INTR;                         // Reiniciar la funcion enableIRIn()
+    irrecv.decode(&results);                   // Recibir la confirmacion del receptor
+  } while(!results.value);                     // Reenviar el paquete hasta que no se encuentre confirmacion
+
+  TIMER_DISABLE_INTR;                          // Desactivar el modulo infrarrojo
+  
+  delay(30);
+
+  sequence++;                                  // Aumentar el numero de secuencia en uno
+  if(sequence >= 1023) sequence = 0;           // Si el numero de secuencia esta a punto de rebasar, reinicia la cuenta
+}
